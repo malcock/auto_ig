ab91a51d96ab9818333b01a8999c5e74f783d62e
Martin  Alcock <martinalcock84@gmail.com>
2018-04-08 21:43:12 +0100

    new day trend matching

--- auto_ig/strategy/wma_cross.py
+++ auto_ig/strategy/wma_cross.py
@@ -32,37 +32,36 @@ class wma_cross(Strategy):
                 
     def fast_signals(self,market,prices,resolution):
         super().fast_signals(market,prices,resolution)
-        stoch, d = ta.stochastic(prices,14,3,3)
-        now = prices[-1]
-        # check for bull closes
-        nowstoch = stoch[-1]
-        maxstoch = max(stoch[-4:])
-        if nowstoch < 75 and maxstoch > 80 and abs(nowstoch-maxstoch)>4:
-            sig = Sig("STOCH_CLOSE",now['snapshotTime'],"SELL",2,life=1)
-            super().add_signal(sig,market)
+        # stoch, d = ta.stochastic(prices,14,3,3)
+        # now = prices[-1]
+        # # check for bull closes
+        # nowstoch = stoch[-1]
+        # maxstoch = max(stoch[-4:])
+        # if nowstoch < 75 and maxstoch > 80 and abs(nowstoch-maxstoch)>4:
+        #     sig = Sig("STOCH_CLOSE",now['snapshotTime'],"SELL",2,life=1)
+        #     super().add_signal(sig,market)
         
-        # check for bear closes
-        minstoch = min(stoch[-4:])
-        if nowstoch > 25 and minstoch < 20 and abs(nowstoch-minstoch)>4:
-            sig = Sig("STOCH_CLOSE",now['snapshotTime'],"BUY",2,life=1)
-            super().add_signal(sig,market)
+        # # check for bear closes
+        # minstoch = min(stoch[-4:])
+        # if nowstoch > 25 and minstoch < 20 and abs(nowstoch-minstoch)>4:
+        #     sig = Sig("STOCH_CLOSE",now['snapshotTime'],"BUY",2,life=1)
+        #     super().add_signal(sig,market)
 
 
     def slow_signals(self,market,prices, resolution):
         super().slow_signals(market,prices,resolution)
         # what's the dailies saying?
-        day_stoch, d = ta.stochastic(market.prices["DAY"],14,3,3)
+        day_k, day_d = ta.stochastic(market.prices["DAY"],14,3,3)
         day_wma25 = ta.wma(25,market.prices['DAY'])
-        day_change = market.net_change
 
         # want to look at the daily trends before even considering opening a position
         daydir = "NONE"
-        stoch_delta = day_stoch[-1] - day_stoch[-2]
+        stoch_delta = day_k[-1] - day_d[-1]
         wma_delta = day_wma25[-1] - day_wma25[-2]
-        if stoch_delta > 0 and wma_delta > 0:
+        if (stoch_delta > 0 or day_k > 85) and wma_delta > 0:
             daydir = "BUY"
         
-        if stoch_delta < 0 and wma_delta < 0:
+        if (stoch_delta < 0 or day_k < 15) and wma_delta < 0:
             daydir = "SELL"
 
         
@@ -78,23 +77,23 @@ class wma_cross(Strategy):
 
             # look for bullish wma signals
             if detect.crossover(fast,slow):
-                sig = Sig("WMA_CROSS",now['snapshotTime'],"BUY",2,life=9)
+                sig = Sig("WMA_CROSS",now['snapshotTime'],"BUY",2,life=7)
                 super().add_signal(sig,market)
 
                 # if we match all open conditions, create an additional CONFIRM signal
-                if (detect.isbelow(trend[-1], now['openPrice']['bid'],now['closePrice']['bid'])):
+                if (detect.isabove(trend[-1], trend[-2])):
                     if daydir=="BUY":
-                        sig = Sig("WMA_CROSS_CONFIRM",now['snapshotTime'],"BUY",4,comment = "confirmed by trend below candle and good rsi",life=1)
+                        sig = Sig("WMA_CROSS_CONFIRM",now['snapshotTime'],"BUY",4,comment = "confirmed by all trends on cross",life=1)
                         super().add_signal(sig,market)
 
             # look for bearish wma signals
             if detect.crossunder(fast,slow):
-                sig = Sig("WMA_CROSS",now['snapshotTime'],"SELL",2,life=9)
+                sig = Sig("WMA_CROSS",now['snapshotTime'],"SELL",2,life=7)
                 super().add_signal(sig,market)
                 # if we match all open conditions, create an additional CONFIRM signal
-                if (detect.isabove(trend[-1], now['openPrice']['bid'],now['closePrice']['bid'])):
+                if (detect.isbelow(trend[-1], trend[-2])):
                     if daydir=="SELL":
-                        sig = Sig("WMA_CROSS_CONFIRM",now['snapshotTime'],"SELL",4,comment = "confirmed by trend below candle", life=1)
+                        sig = Sig("WMA_CROSS_CONFIRM",now['snapshotTime'],"SELL",4,comment = "confirmed by all trends on cross", life=1)
                         super().add_signal(sig,market)
                 
 
@@ -102,14 +101,14 @@ class wma_cross(Strategy):
             cross_sigs = [x for x in self.signals if x.name=="WMA_CROSS" and x.market==market.epic]
             for s in cross_sigs:
                 if s.position=="BUY":
-                    if detect.candleover(trend,prices) and daydir=="BUY":
+                    if detect.isabove(trend[-1], trend[-2]) and daydir=="BUY":
                         sigC = Sig("WMA_CONFIRM",now['snapshotTime'],"BUY",4)
-                        sigC.comment = "confirmed by candle over"
+                        sigC.comment = "confirmed by trend"
                         super().add_signal(sigC,market)
                 else:
-                    if detect.candleunder(trend,prices) and daydir=="SELL":
+                    if detect.isbelow(trend[-1], trend[-2]) and daydir=="SELL":
                         sigC = Sig("WMA_CONFIRM",now['snapshotTime'],"SELL",4)
-                        sigC.comment = "confirmed by candle under"
+                        sigC.comment = "confirmed by trend"
                         super().add_signal(sigC,market)
 
 
