c85f0df624b3622dbd76106828a24c2a66ea6199
Martin  Alcock <martinalcock84@gmail.com>
2018-05-15 10:00:43 +0100

    removed the ma check from opener

--- auto_ig/strategy/stoch.py
+++ auto_ig/strategy/stoch.py
@@ -248,12 +248,12 @@ class stoch(Strategy):
             ma7_delta = ma7[-1] - ma7[-2]
             for s in threshold_sigs:
                 if s.position=="BUY":
-                    if ema5_delta > 0 and ma7_delta > 0:
+                    if ema5_delta > 0.5:
                         sig = Sig("STOCH_SLOW_OPEN",now['snapshotTime'],"BUY",4,comment="stars have aligned 30 min; {} {}".format(s.timestamp, s.comment),life=2)
                         super().add_signal(sig,market)
                         self.signals.remove(s)
                 else:
-                    if ema5_delta < 0 and ma7_delta < 0:
+                    if ema5_delta < -0.5:
                         sig = Sig("STOCH_SLOW_OPEN",now['snapshotTime'],"SELL",4,comment="stars have aligned 30 min; {} {}".format(s.timestamp, s.comment),life=2)
                         super().add_signal(sig,market)
                         self.signals.remove(s)
